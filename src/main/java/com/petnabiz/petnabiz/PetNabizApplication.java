04bad59b70f9d5067ce4de08cd3e081e315f5a11
Serhat Aydın <147271155+serhaydin@users.noreply.github.com>
2025-11-21 16:24:02 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/petnabiz/petnabiz/PetNabizApplication.java
@@ -0,0 +1,13 @@
+package com.petnabiz.petnabiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetNabizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetNabizApplication.class, args);
+	}
+
+}
